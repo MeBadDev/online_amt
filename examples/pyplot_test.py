02dfe0eb26785b372e810dbe7a6467d299bfd7f2
MeBadDev <94725241+MeBadDev@users.noreply.github.com>
2025-06-16 17:15:30 +0800

    Properly load librarys for examples

--- examples/pyplot_test.py
+++ examples/pyplot_test.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
--- examples/run_on_web.py
+++ examples/run_on_web.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flask import Flask, render_template, jsonify
 import pyaudio
 from transcribe import load_model, OnlineTranscriber
